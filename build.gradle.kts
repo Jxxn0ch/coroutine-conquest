4f8c8b8cef70bcc3a3fcfa5b2b4ade9b0ed59b0d
junyoung.choi <junyoung.choi@kurlycorp.com>
2024-11-12 07:53:25 +0900

    gradle정보 재생성

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "com.study"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
---
+++ src/main/kotlin/section7/ParentChildrenJob.kt
@@ -0,0 +1,13 @@
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking<Unit> {
+  val parentJob: Job? = coroutineContext[Job]
+  launch {
+    val childJob: Job? = coroutineContext[Job]
+//    println("1. 자식 코루틴의 Job이 가지고 있는 parent는 부모 코루틴의 Job인가? ${childJob?.parent === parentJob}")
+    println("2. 부모 코루틴의 Job은 자식 코루틴의 Job을 참조를 가지는가? ${parentJob?.children?.contains(childJob)}")
+  }
+
+}
